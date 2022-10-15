a97acbdafcc360701e67d1d4966e7730cdcaa73c
Isabelle-Grimesey <41082201+Isabelle-Grimesey@users.noreply.github.com>
2022-10-15 05:59:35 -0500

    Update find_malicious_packages.sh

--- find_malicious_packages.sh
+++ find_malicious_packages.sh
@@ -1,1 +1,5 @@
-apt list -a package_name
+#!/usr/bin/bash
+#asks apt package manager (ubuntu, debian) if any packages exist whose names have an exact match in the malicious package name blacklist.
+for str in "nmap" "zenmap" "apache2" "nginx" "lighttpd" "wireshark" "tcpdump" "netcat-traditional" "nikto" "ophcrack" "; do
+	apt list -a $str
+done

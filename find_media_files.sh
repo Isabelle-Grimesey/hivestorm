68e29fd434ab9ec33494af17c4d915102e4fdc41
Isabelle-Grimesey <41082201+Isabelle-Grimesey@users.noreply.github.com>
2022-10-15 05:26:50 -0500

    Update find_media_files.sh

--- find_media_files.sh
+++ find_media_files.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
 #searches entire file system for the most common audio and video file extensions. https://fileinfo.com/filetypes/common
-for str in "*.aif" "*.flac" "*.m3u" "*.m4a" "*.mid" "*.mp3" "*.ogg" "*.wav" "*.wma" "*.aif" "*.m4b" "*.3gp" "*.asf" "*.avi" "*.flv" "*.m4v" "*.mov" "*.mp4" "*.mpg" "*.srt" "*.swf" "*.ts" "*.vob" "*.wmv"; do
+for str in "*.aif" "*.flac" "*.m3u" "*.m4a" "*.mid" "*.mp3" "*.ogg" "*.wav" "*.wma" "*.aif" "*.m4b" "*.3gp" "*.asf" "*.avi" "*.flv" "*.m4v" "*.mov" "*.mp4" "*.mpg" "*.srt" "*.swf" "*.ts" "*.vob" "*.wmv" "*.mkv" "*.f4v" "*.avchd"; do
 	find / -type f -name $str
 done
